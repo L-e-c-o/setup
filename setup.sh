42796beca3023bb59435105ce5298bb58186a0b4
Leco <60701459+L-e-c-o@users.noreply.github.com>
2021-09-26 16:29:40 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update and remove unnecessary packages
+apt update && apt upgrade -y && apt autoremove -y
+
+# Install necessary packages
+apt install tmux trash-cli neovim xclip zsh golang gobuster ftp 
+
+# Install oh-my-zsh
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Autosuggestions & syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Fetch configuration files
+git clone git@github.com:L-e-c-o/setup.git
+cd setup
+cp .zshrf $HOME/.zshrc
+cp -r nvim $HOME/.config/
+cp tmux.conf $HOME/.tmux.conf
+
+# Cleanup
+cd .. && rm -rf setup
+
+source $HOME/.zshrc
