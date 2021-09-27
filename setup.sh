eab9ed5234bfdf1f1adbde6b234724ce9ec75645
Leco <60701459+L-e-c-o@users.noreply.github.com>
2021-09-27 12:05:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,6 @@ apt install tmux trash-cli neovim xclip zsh golang gobuster ftp -y
 
 # Install oh-my-zsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-zsh
-
-# Autosuggestions & syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Fetch configuration files
 git clone https://github.com/L-e-c-o/setup.git
@@ -22,7 +17,14 @@ cp -r nvim $HOME/.config/
 cp .tmux.conf $HOME/.tmux.conf
 cp .tmux.conf.local $HOME/.tmux.conf.local
 
+# Autosuggestions & syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
 source $HOME/.zshrc
+chsh -s /bin/zsh
+
+touch ~/.hushlogin
 
 # Cleanup
 cd .. 
