dbe285b8dd6c3610250bc9bbf724ddee80bec800
Leco <60701459+L-e-c-o@users.noreply.github.com>
2021-09-27 11:01:10 +0200

    tests

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Update and remove unnecessary packages
-apt update && apt upgrade -y && apt autoremove -y
+#apt update && apt upgrade -y && apt autoremove -y
 
 # Install necessary packages
-apt install tmux trash-cli neovim xclip zsh golang gobuster ftp -y
+#apt install tmux trash-cli neovim xclip zsh golang gobuster ftp -y
 
 # Install oh-my-zsh
-yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Autosuggestions & syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Fetch configuration files
 https://github.com/L-e-c-o/setup.git
 cd setup
-cp .zshrf $HOME/.zshrc
+cp .zshrc $HOME/.zshrc
 cp -r nvim $HOME/.config/
 cp tmux.conf $HOME/.tmux.conf
 
